2a4ba1ed0e6c3d9cde049f168b799cd4567c212b
K4CZP3R <kacper@kacperserewis.net>
2021-11-04 22:21:34 +0100

    categorize rules and add regex :D

--- main.py
+++ main.py
@@ -3,21 +3,14 @@ from helpers.file_loader import FileLoader
 
 from helpers.file_loader import FileLoader
 from helpers.yaml_loader import YamlLoader
+from helpers.yaml_rules_collections import YamlRulesCollections
 from models.yaml_rule import YamlRule
 from services.compose_checker import ComposeChecker
 
 docker_compose = YamlLoader.load_yaml_from_str(
     FileLoader.load_file_to_str("docker-compose.yml"))
 
-rules = [
-    YamlRule("networks", should_contain="ksp-traefik"),
-    YamlRule("networks.ksp-traefik.external", should_be=True),
-    YamlRule("services.?.networks", should_contain="ksp-traefik"),
-    YamlRule("services.*.deploy", should_contain="replicas"),
-    YamlRule("services.?.deploy.labels", should_contain="traefik.enable=true"),
-    YamlRule("services.?.deploy.labels",
-             should_contain="traefik.docker.network=ksp-traefik")
-]
-
+rules = [*YamlRulesCollections.get_traefik_rules(), *YamlRulesCollections.get_swarm_rules(),
+         *YamlRulesCollections.get_network_rules("ksp-traefik"), *YamlRulesCollections.get_flame_rules(), *YamlRulesCollections.get_version_rules()]
 
 ComposeChecker(docker_compose, rules).check()
